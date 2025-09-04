737504db087700ff12ab5c71ce1f60f9a03b92db
Vikramaditya <vikramadityapratap35@gmail.com>
2025-09-04 14:39:54 +0530

    comment updated

--- main.py
+++ main.py
@@ -14,11 +14,13 @@ with open("class_indices.json", "r") as f:
     class_indices = json.load(f)
 class_names = {v: k for k, v in class_indices.items()}  # reverse mapping
 
+# prepocessing the image
 def preprocess(image):
     image = image.resize((224, 224))
     img_array = np.array(image) / 255.0
     return np.expand_dims(img_array, axis=0)
 
+# endpoint 
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
     image = Image.open(file.file).convert("RGB")
@@ -29,3 +31,4 @@ async def predict(file: UploadFile = File(...)):
         "disease": class_names[idx],
         "confidence": float(np.max(predictions))
     }
+
